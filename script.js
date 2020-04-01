bdb12c47f29cbeadf0d0f286c7b37a6476ff4055
rjrahul98 <rjrahul1711@gmail.com>
2020-04-01 13:07:08 +0530

    three files added

---
+++ script.js
@@ -0,0 +1,61 @@
+const particles = [];
+
+function setup(){
+    createCanvas(window.innerWidth, window.innerHeight);
+    const particlesLength = Math.floor(window.innerWidth/9);
+    
+    for(let i=0; i< particlesLength; i++)
+    {
+        particles.push(new Particle());
+    }
+}
+
+function draw(){
+    background(115,64,156); 
+    particles.forEach((p,index)=>{
+        p.update();
+        p.draw();
+        p.checkParticles(particles.slice(index));
+    });
+}
+
+class Particle{
+    constructor(){
+        //Position
+        this.pos = createVector(random(width),random(height));
+        //Velocity
+        this.vel = createVector(random(-2,2),random(-2,2));
+        //Size
+        this.size = 8;
+    }
+
+    update(){
+        this.pos.add(this.vel);
+        this.edges();
+    }
+
+    draw(){
+        noStroke();
+        fill('rgba(255,255,255,0.3)');
+        circle(this.pos.x,this.pos.y,this.size);
+    }
+
+    edges(){
+        if(this.pos.x<0 || this.pos.x>width){
+            this.vel.x*=-1;
+        }
+        if(this.pos.y<0 || this.pos.y>height){
+            this.vel.y*=-1;
+        }
+    }
+
+    checkParticles(particles){
+        particles.forEach(particle =>{
+            const d = dist(this.pos.x,this.pos.y,particle.pos.x,particle.pos.y);
+            if(d<120){
+                stroke('rgba(255,255,255,0.1)');
+                line(this.pos.x,this.pos.y,particle.pos.x,particle.pos.y);
+            }
+        })
+    }
+}
